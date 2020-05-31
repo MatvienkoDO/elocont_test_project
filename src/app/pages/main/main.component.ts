60b478c2aaf6a68e3b4425908aa4b0bb4aaeded8
MatvienkoDO <chastor97@gmail.com>
2020-06-01 01:41:34 +0400

    implements a bit of main component

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { DataItem } from '../../interfaces/data-item';
 
 @Component({
   selector: 'app-main',
@@ -6,10 +10,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  public readonly fields = fields;
 
-  constructor() { }
+  public items$?: Observable<DataItem[]>;
+
+  constructor(private readonly http: HttpClient) { }
 
   ngOnInit() {
+    this.items$ = this.http.get<DataItem[]>('table-data');
   }
 
 }
+
+const fields = [
+  'healthIndex',
+  'endDate',
+  'minValueDateTime',
+  'type',
+  'cowId',
+  'animalId',
+  'eventId',
+  'deletable',
+  'lactationNumber',
+  'daysInLactation',
+  'ageInDays',
+  'startDateTime',
+  'reportingDateTime',
+  'alertType',
+  'duration',
+  'originalStartDateTime',
+  'endDateTime',
+  'daysInPregnancy',
+  'destinationGroup',
+  'destinationGroupName',
+  'calvingEase',
+  'oldLactationNumber',
+  'newborns',
+  'heatIndexPeak',
+  'newGroupId',
+  'newGroupName',
+  'currentGroupId',
+  'currentGroupName'
+];
