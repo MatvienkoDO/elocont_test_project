770747eef9df4c84f5b393e1c4b48168c8473fd0
MatvienkoDO <chastor97@gmail.com>
2020-05-31 20:28:44 +0400

    implements interceptor for mocking table-data get request

--- src/app/interceptors/table-data.interceptor.ts
+++ src/app/interceptors/table-data.interceptor.ts
@@ -1,13 +1,35 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse } from '@angular/common/http';
+import { Observable, ReplaySubject } from 'rxjs';
+import { map, first } from 'rxjs/operators';
 
 @Injectable()
 export class TableDataInterceptor implements HttpInterceptor {
-  constructor() { }
+  private readonly data = new ReplaySubject(1);
+
+  constructor() {
+    fetch("assets/data.json").then(async response => {
+      const data = await response.json();
+      
+      if (data && Array.isArray(data.result)) {
+        this.data.next(data.result);
+      }
+    });
+  }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    throw new Error("Method not implemented.");
-    // return next.handle(req);
+    if (req.method === 'get' && req.url === 'table-data') {
+
+      return this.data.pipe(
+        first(),
+        map(
+          data => new HttpResponse({
+            body: data
+          })
+        )
+      );
+    }
+
+    return next.handle(req);
   }
 }
