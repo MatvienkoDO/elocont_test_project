90657762e82f171a8162f2c148c207e325f9eba7
MatvienkoDO <chastor97@gmail.com>
2020-06-01 02:30:57 +0400

    usage of Column

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { DataItem } from '../../interfaces/data-item';
+import { Column } from '../../interfaces/column';
 
 @Component({
   selector: 'app-main',
@@ -10,7 +11,7 @@ import { DataItem } from '../../interfaces/data-item';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-  public readonly fields = fields;
+  public readonly columns = columns;
 
   public items$?: Observable<DataItem[]>;
 
@@ -22,33 +23,33 @@ export class MainComponent implements OnInit {
 
 }
 
-const fields = [
-  'healthIndex',
-  'endDate',
-  'minValueDateTime',
-  'type',
-  'cowId',
-  'animalId',
-  'eventId',
-  'deletable',
-  'lactationNumber',
-  'daysInLactation',
-  'ageInDays',
-  'startDateTime',
-  'reportingDateTime',
-  'alertType',
-  'duration',
-  'originalStartDateTime',
-  'endDateTime',
-  'daysInPregnancy',
-  'destinationGroup',
-  'destinationGroupName',
-  'calvingEase',
-  'oldLactationNumber',
-  'newborns',
-  'heatIndexPeak',
-  'newGroupId',
-  'newGroupName',
-  'currentGroupId',
-  'currentGroupName'
+const columns: Column[] = [
+  { key: 'healthIndex', name: 'Health index' },
+  { key: 'endDate', name: 'End date' },
+  { key: 'minValueDateTime', name: 'MinValue datetime' },
+  { key: 'type', name: 'Type' },
+  { key: 'cowId', name: 'Cow id' },
+  { key: 'animalId', name: 'Animal id' },
+  { key: 'eventId', name: 'Event id' },
+  { key: 'deletable', name: 'Deletable' },
+  { key: 'lactationNumber', name: 'Lactation number' },
+  { key: 'daysInLactation', name: 'Days in lactation' },
+  { key: 'ageInDays', name: 'Age in days' },
+  { key: 'startDateTime', name: 'Start datetime' },
+  { key: 'reportingDateTime', name: 'Reporting datetime' },
+  { key: 'alertType', name: 'Alert type' },
+  { key: 'duration', name: 'Duration' },
+  { key: 'originalStartDateTime', name: 'Original start datetime' },
+  { key: 'endDateTime', name: 'End datetime' },
+  { key: 'daysInPregnancy', name: 'Days in pregnancy' },
+  { key: 'destinationGroup', name: 'Destination group' },
+  { key: 'destinationGroupName', name: 'Destination group name' },
+  { key: 'calvingEase', name: 'Calving ease' },
+  { key: 'oldLactationNumber', name: 'Old lactation number' },
+  { key: 'newborns', name: 'Newborns' },
+  { key: 'heatIndexPeak', name: 'Heat index peak' },
+  { key: 'newGroupId', name: 'New group id' },
+  { key: 'newGroupName', name: 'New group name' },
+  { key: 'currentGroupId', name: 'Current group id' },
+  { key: 'currentGroupName', name: 'Current group name' }
 ];
