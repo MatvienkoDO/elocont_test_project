3278678d8bea2c5f7edd23e208626c63cd51ebf6
MatvienkoDO <chastor97@gmail.com>
2020-05-31 16:51:44 +0400

    adjust injecting settings

--- src/app/interceptors/table-data.interceptor.ts
+++ src/app/interceptors/table-data.interceptor.ts
@@ -2,14 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class TableDataInterceptor implements HttpInterceptor {
-
   constructor() { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     throw new Error("Method not implemented.");
+    // return next.handle(req);
   }
 }
