8ecaf467ed124d8c9dac28d13379fcc1edbd06eb
MatvienkoDO <chastor97@gmail.com>
2020-06-01 06:36:11 +0400

    implements cell 'edit' functionality

--- src/app/components/cell/cell.component.ts
+++ src/app/components/cell/cell.component.ts
@@ -1,16 +1,55 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-cell',
   templateUrl: './cell.component.html',
-  styleUrls: ['./cell.component.scss']
+  styleUrls: ['./cell.component.scss'],
+  host: { '(click)': 'onClickWholeCell()' }
 })
-export class CellComponent implements OnInit {
+export class CellComponent implements OnInit, OnChanges {
   @Input() data: any = undefined;
+  @Input() changable = true;
+
+  @Output() update = new EventEmitter<any>();
+
+  public isChanging = false;
 
   constructor() { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const changable = changes.changable;
+
+    if (changable && changable.previousValue && !changable.currentValue) {
+      this.isChanging = false;
+    }
+  }
 
   ngOnInit() {
   }
 
+  onClickWholeCell() {
+    // markForCheck
+    this.isChanging = true;
+  }
+
+  onInputLoseFocus() {
+    // markForCheck
+    this.isChanging = false;
+  }
+
+  submit(event) {
+    // markForCheck
+    this.isChanging = false;
+
+    const value = event.target[0].value;
+
+    if (value != this.data) {
+      this.data = value;
+      this.update.emit(value);
+    }
+
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
+  }
 }
