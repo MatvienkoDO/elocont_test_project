2e69cb4a509a44a28055b2db0175a232f59a53f7
MatvienkoDO <chastor97@gmail.com>
2020-06-01 04:05:58 +0400

    removes redundant code

--- src/app/components/row/row.component.ts
+++ src/app/components/row/row.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, HostBinding, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { Column } from '../../interfaces/column';
 
@@ -7,24 +7,15 @@ import { Column } from '../../interfaces/column';
   templateUrl: './row.component.html',
   styleUrls: ['./row.component.scss']
 })
-export class RowComponent implements OnInit, OnChanges {
+export class RowComponent implements OnInit {
   @Input() index: number = NaN;
   @Input() columns: Column[] = [];
   @Input() data: object = {};
   @Input('is-header') isHeader = false;
   @Input() deletable = true;
 
-  @HostBinding('class.deletable') deletableClass = true;
-
   constructor() { }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes.deletable) {
-      this.deletableClass = changes.deletable.currentValue;
-    }
-  }
 
   ngOnInit() {
   }
-
 }
